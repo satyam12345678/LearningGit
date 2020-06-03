2c95ba359fb24d9a0fec083909c59443ad587760
Satyam Tripathi <sk729485@gmail.com>
2020-06-03 19:27:26 +0530

    Initial RGB

---
+++ lissRGB.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+	string acc,ins;
+	cin>>acc>>ins;
+	int cnt=1;
+	for(int i=0,j=0;i<acc.length(),j<ins.length();j++)
+	{
+		if(acc[i]==ins[j])
+		{
+			cnt++;
+			i++;
+		}
+	}
+	cout<<cnt;
+}
